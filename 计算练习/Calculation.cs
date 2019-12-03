22e5046713eb353da6939e72f183cdaa8b77986a
jeuo <zhao_jing_1981@hotmail.com>
2019-12-03 11:37:03 +0800

    test

---
+++ 计算练习/Calculation.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 计算练习
+{
+    public class Calculation
+    {
+        public const string 加 = "＋";
+        public const string 减 = "－";
+        public const string 乘 = "×";
+        public const string 除 = "÷";
+        public static Random r = new Random();
+
+        public static void 出题<T>(int count, Func<T> fn, List<T> result)
+        {
+            int i = 0;
+            while (i++ < count)
+            {
+                if(fn != null) result.Add(fn.Invoke());
+            }
+        }
+
+        public static void 出题<T>(int count, Func<int, T> fn, int arg, List<T> result)
+        {
+            int i = 0;
+            while (i++ < count)
+            {
+                if (fn != null) result.Add(fn.Invoke(arg));
+            }
+        }
+
+        public static void 出题<T>(int count, Func<int, bool, T> fn, int arg0, bool arg1, List<T> result)
+        {
+            int i = 0;
+            while (i++ < count)
+            {
+                if (fn != null) result.Add(fn.Invoke(arg0, arg1));
+            }
+        }
+
+        public static List<T> RandomSortList<T>(List<T> ListT)
+        {
+            Random random = new Random();
+            List<T> newList = new List<T>();
+            foreach (T item in ListT)
+            {
+                newList.Insert(random.Next(newList.Count + 1), item);
+            }
+            return newList;
+        }
+
+        public static string 二十以内加减法()
+        {
+            int n1, n2;
+            if (r.Next(2) == 1)
+            {
+                do
+                {
+                    n1 = r.Next(1, 20);
+                    n2 = r.Next(2, 20);
+                } while (n1 + n2 > 20);
+                return n1 + 加 + n2;
+            }
+            else
+            {
+                n1 = r.Next(3, 21);
+                n2 = r.Next(2, n1);
+                return n1 + 减 + n2;
+            }
+        }
+
+        public static string 两位数加减个位()
+        {
+            int n1, n2;
+            if (r.Next(2) == 1)
+            {
+                do
+                {
+                    n1 = r.Next(1, 10);
+                    n2 = r.Next(2, 10);
+                } while (n1 + n2 >= 10);
+                n1 = r.Next(1, 10) * 10 + n1;
+                if (r.Next(2) == 1) return n1 + "＋" + n2;
+                else return n2 + 加 + n1;
+            }
+            else
+            {
+                n1 = r.Next(2, 10);
+                n2 = r.Next(2, n1 + 1);
+                n1 = r.Next(1, 10) * 10 + n1;
+                return n1 + 减 + n2;
+            }
+        }
+
+        public static string 两位数加减整十()
+        {
+            int n1, n2;
+            if (r.Next(2) == 1)
+            {
+                do
+                {
+                    n1 = r.Next(1, 10);
+                    n2 = r.Next(1, 10);
+                } while (n1 + n2 >= 10);
+                n1 = n1 * 10 + r.Next(1, 10);
+                n2 = n2 * 10;
+                if (r.Next(2) == 1) return n1 + 加 + n2;
+                else return n2 + 加 + n1;
+            }
+            else
+            {
+                n1 = r.Next(2, 10);
+                n2 = r.Next(1, n1 + 1);
+                n1 = n1 * 10 + r.Next(10);
+                n2 = n2 * 10;
+                return n1 + 减 + n2;
+            }
+        }
+
+        public static string X以内乘法(int limit)
+        {
+            return r.Next(2,limit+1) + 乘 + r.Next(2, limit + 1);
+        }
+
+        public static string X以内除法(int limit)
+        {
+            int n1 = r.Next(2, limit + 1);
+            return n1* r.Next(2, limit + 1) + 除 + n1;
+        }
+
+        public static string X以内加法(int limit, bool uncarry = false)
+        {
+            int n1 = r.Next(1, limit);
+            int n2 = r.Next(1, limit - n1 + 1);
+            if (uncarry)
+            {
+                while (IsCarry(n1, n2))
+                {
+                    n1 = r.Next(1, limit);
+                    n2 = r.Next(1, limit - n1 + 1);
+                }
+            }
+            return n1 + 加 + n2;
+        }
+
+        public static string X以内减法(int limit, bool unborrow = false)
+        {
+            int n1 = r.Next(2, limit + 1);
+            int n2 = r.Next(1, n1);
+            if (unborrow)
+            {
+                while (IsBorrow(n1, n2))
+                {
+                    n1 = r.Next(2, limit + 1);
+                    n2 = r.Next(1, n1);
+                }
+            }
+            return n1 + 减 + n2;
+        }
+
+        public static bool IsBorrow(int n1, int n2)
+        {
+            string s1 = n1.ToString();
+            string s2 = n2.ToString();
+            while (s1.Length > 0 && s2.Length > 0)
+            {
+                if (s1[s1.Length - 1] < s2[s2.Length - 1]) return true;
+                s1 = s1.Remove(s1.Length - 1, 1);
+                s2 = s2.Remove(s2.Length - 1, 1);
+            }
+            return false;
+        }
+
+        public static bool IsCarry(int n1, int n2)
+        {
+            return IsBorrow(n1 + n2, n1);
+        }
+    }
+}
---
+++ 计算练习/Window1.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace 计算练习
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        public void Export(List<string> result, string path, string title= "计算练习", string filename= "计算练习")
+        {
+            path = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"<html><HEAD><META http-equiv=Content-Type content='text/html; charset=gb2312'></head><body><center><h2>{title}</h2><h3></h3>班级__________ 姓名________ 学号__________<br><br><table border=0 width=640 cellspacing=10>");
+            for (int i = 0; i < result.Count; i++)
+            {
+                if (i == 0) sb.Append("<tr>");
+                else if (i % 4 == 0) sb.Append("</tr><tr>");
+                sb.Append("<td>" + result[i] + "＝</td>");
+            }
+            sb.Append("</tr><tr></tr></table></center></body></html>");
+            if (File.Exists(System.IO.Path.Combine(path, $"{filename}.htm")))
+            {
+                int i = 1;
+                while (File.Exists(System.IO.Path.Combine(path, $"{filename}{i}.htm")))
+                {
+                    i++;
+                }
+                filename = filename + i;
+            }
+            File.WriteAllText(System.IO.Path.Combine(path, $"{filename}.htm"), sb.ToString(), Encoding.Unicode);
+            Process.Start(System.IO.Path.Combine(path, $"{filename}.htm"));
+        }
+
+        private void btn1b_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> result = new List<string>();
+            Calculation.出题(Convert.ToInt16(txt1bt1.Text), Calculation.二十以内加减法, result);
+            Calculation.出题(Convert.ToInt16(txt1bt2.Text), Calculation.两位数加减个位, result);
+            Calculation.出题(Convert.ToInt16(txt1bt3.Text), Calculation.两位数加减整十, result);
+            result = Calculation.RandomSortList<string>(result);
+            Export(result, null);
+        }
+
+        private void btn2a_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> result = new List<string>();
+            Calculation.出题(Convert.ToInt16(t1.Text), Calculation.X以内乘法, Convert.ToInt16(s1.Text), result);
+            Calculation.出题(Convert.ToInt16(t2.Text), Calculation.X以内除法, Convert.ToInt16(s2.Text), result);
+            Calculation.出题(Convert.ToInt16(t3.Text), Calculation.X以内加法, Convert.ToInt16(s3.Text), chkUnCarry.IsChecked ?? false, result);
+            Calculation.出题(Convert.ToInt16(t4.Text), Calculation.X以内减法, Convert.ToInt16(s4.Text), chkUnBorrow.IsChecked ?? false, result);
+            Calculation.出题(Convert.ToInt16(t5.Text), Calculation.两位数加减个位, result);
+            Calculation.出题(Convert.ToInt16(t6.Text), Calculation.两位数加减整十, result);
+            Calculation.出题(Convert.ToInt16(t7.Text), Calculation.二十以内加减法, result);
+
+            result = Calculation.RandomSortList<string>(result);
+            Export(result, null);
+        }
+    }
+}
